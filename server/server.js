b2b7d1e83fafb4dc90297febac940f1e0ffb8e0a
Vahe <vaheyavrumian@gmail.com>
2018-11-04 17:39:42 +0400

    Greet new User and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: "admin",
+		text: "Welcome govnyuk",
+		createdAt: new Date().getTime()
+	});
+	socket.broadcast.emit('newMessage', {
+		from: "Admin",
+		text: "Govnyuk joined",
+		createdAt: new Date().getTime()
+	})
 	socket.on('createMessage', (message) => {
-		console.log('createMessage', message);
+
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
 		})
+
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// })
 	});
 
 
