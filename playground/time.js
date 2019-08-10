86d77bf240a80684a4746750b7a7307581a7a0ee
Vahe Yavrumian <yavrumian@protonmail.com>
2019-08-10 21:11:17 +0400

    Deleted playground folder

--- playground/time.js
+++
@@ -1,9 +0,0 @@
-var moment = require('moment');
-
-// var date = moment();
-// console.log(date.format('MMM Do YYYY'))
-
-
-var createdAt = 1234;
-var date = moment(createdAt);
-console.log(date.format('h:mm a'))
